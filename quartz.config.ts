378bc08329f145215fe8b03b2d328dd11a1f2eaf
Race Williams <race@raquent.in>
2025-10-15 18:03:25 -0400

    color

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,87 +7,87 @@ import * as Plugin from "./quartz/plugins"
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
 const config: QuartzConfig = {
-  configuration: {
-    pageTitle: "Raquent.in",
-    pageTitleSuffix: "",
-    enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "none",
-    },
-    locale: "en-US",
-    baseUrl: "raquent.in",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
-    generateSocialImages: false,
-    theme: {
-      fontOrigin: "googleFonts",
-      cdnCaching: true,
-      typography: {
-        header: "DM Serif Display",
-        body: "Inter",
-        code: "DM Mono",
-      },
-      colors: {
-        lightMode: {
-          light: "#0f0f0f",
-          lightgray: "#303030",
-          gray: "#dddddd",
-          darkgray: "#e4e4e4",
-          dark: "#ffffff",
-          secondary: "#d1d1d1",
-          tertiary: "#5e5e5e",
-          highlight: "rgba(84, 84, 84, 0.22)",
+    configuration: {
+        pageTitle: "Raquent.in",
+        pageTitleSuffix: "",
+        enableSPA: true,
+        enablePopovers: true,
+        analytics: {
+            provider: "none",
         },
-        darkMode: {
-          light: "#0f0f0f",
-          lightgray: "#303030",
-          gray: "#dddddd",
-          darkgray: "#e4e4e4",
-          dark: "#ffffff",
-          secondary: "#d1d1d1",
-          tertiary: "#5e5e5e",
-          highlight: "rgba(84, 84, 84, 0.22)",
-        },
-      },
-    },
-  },
-  plugins: {
-    transformers: [
-      Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
-      }),
-      Plugin.SyntaxHighlighting({
+        locale: "en-US",
+        baseUrl: "raquent.in",
+        ignorePatterns: ["private", "templates", ".obsidian"],
+        defaultDateType: "created",
+        generateSocialImages: false,
         theme: {
-          light: "vitesse-dark",
-          dark: "vitesse-dark",
+            fontOrigin: "googleFonts",
+            cdnCaching: true,
+            typography: {
+                header: "DM Serif Display",
+                body: "Inter",
+                code: "DM Mono",
+            },
+            colors: {
+                lightMode: {
+                    light: "#161616",
+                    lightgray: "#303030",
+                    gray: "#dddddd",
+                    darkgray: "#e4e4e4",
+                    dark: "#ffffff",
+                    secondary: "#8ab1ed",
+                    tertiary: "#5e5e5e",
+                    highlight: "rgba(84, 84, 84, 0.22)",
+                },
+                darkMode: {
+                    light: "#0f0f0f",
+                    lightgray: "#303030",
+                    gray: "#dddddd",
+                    darkgray: "#e4e4e4",
+                    dark: "#ffffff",
+                    secondary: "#d1d1d1",
+                    tertiary: "#5e5e5e",
+                    highlight: "rgba(84, 84, 84, 0.22)",
+                },
+            },
         },
-        keepBackground: true,
-      }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
-    ],
-    filters: [Plugin.RemoveDrafts()],
-    emitters: [
-      Plugin.AliasRedirects(),
-      Plugin.ComponentResources(),
-      Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
-      Plugin.ContentIndex({
-        enableSiteMap: true,
-        enableRSS: true,
-      }),
-      Plugin.Assets(),
-      Plugin.Static(),
-      Plugin.NotFoundPage(),
-    ],
-  },
+    },
+    plugins: {
+        transformers: [
+            Plugin.FrontMatter(),
+            Plugin.CreatedModifiedDate({
+                priority: ["frontmatter", "filesystem"],
+            }),
+            Plugin.SyntaxHighlighting({
+                theme: {
+                    light: "vitesse-dark",
+                    dark: "vitesse-dark",
+                },
+                keepBackground: true,
+            }),
+            Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+            Plugin.GitHubFlavoredMarkdown(),
+            Plugin.TableOfContents(),
+            Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+            Plugin.Description(),
+            Plugin.Latex({ renderEngine: "katex" }),
+        ],
+        filters: [Plugin.RemoveDrafts()],
+        emitters: [
+            Plugin.AliasRedirects(),
+            Plugin.ComponentResources(),
+            Plugin.ContentPage(),
+            Plugin.FolderPage(),
+            Plugin.TagPage(),
+            Plugin.ContentIndex({
+                enableSiteMap: true,
+                enableRSS: true,
+            }),
+            Plugin.Assets(),
+            Plugin.Static(),
+            Plugin.NotFoundPage(),
+        ],
+    },
 }
 
 export default config
